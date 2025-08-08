27a5e668755e61eaf8373827122465cb876153bd
Hamza BRAVO <16.hamzabravo@gmail.com>
2025-08-08 19:44:31 +0300

    paketlere fiyat değeri fieldı eklendi

--- PossiLicence/Entityies/PackageTypeDbEntity.cs
+++ PossiLicence/Entityies/PackageTypeDbEntity.cs
@@ -5,6 +5,7 @@ public sealed class PackageTypeDbEntity:BaseDbEntity
     public string Caption { get; set; } // paket başlığı
     public int MonthCount { get; set; } // ay sayısı
     public int? DayCount { get; set; } // gün sayısı (opsiyonel)
+    public decimal Price { get; set; } // paket fiyatı
     public string Description { get; set; } // paket açıklaması
     public bool IsDeleted { get; set; } = false; // silinmiş mi
 }
